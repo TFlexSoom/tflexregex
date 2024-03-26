2717b68592fbc14b61bc4c0ca687f67a182cd79c
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2024-03-25 22:58:08 -0700

    Change Up Progression

--- lib/tflexregex/progression.go
+++ lib/tflexregex/progression.go
@@ -1,58 +1,76 @@
 package tflexregex
 
-type CharRangeProgression struct {
-	binaryTree []map[uint]bool
+import (
+	"fmt"
+)
+
+type Progression struct {
+	binaryTreeOfSets []map[uint]bool
 }
 
 const defaultRowCapacity uint = 8
 
-func New() CharRangeProgression {
-	return CharRangeProgression{
-		binaryTree: make([]map[uint]bool, 512),
+func NewProgression() Progression {
+	return Progression{
+		binaryTreeOfSets: make([]map[uint]bool, 511),
 	}
 }
 
-func (crp *CharRangeProgression) TransitionOnCharacter(charVal byte, item uint) {
-	index := uint16(charVal) + 256
-	if (*crp).binaryTree[index] == nil {
-		(*crp).binaryTree[index] = make(map[uint]bool, defaultRowCapacity)
+func (crp *Progression) setTransition(index uint, nextState uint) {
+	if (*crp).binaryTreeOfSets[index] == nil {
+		(*crp).binaryTreeOfSets[index] = make(map[uint]bool, defaultRowCapacity)
 	}
 
-	(*crp).binaryTree[index][item] = true
+	(*crp).binaryTreeOfSets[index][nextState] = true
 }
 
-func (crp *CharRangeProgression) TransitionOnRange(greaterToOrEqual byte, lessThan byte, item uint) {
-	if greaterToOrEqual >= lessThan {
-		return
-	} else if greaterToOrEqual == lessThan-1 {
-		crp.TransitionOnCharacter(greaterToOrEqual, item)
-		return
+func (crp *Progression) TransitionOnCharacter(charVal byte, nextState uint) {
+	crp.setTransition(uint(charVal)+255, nextState)
+}
+
+func (crp *Progression) TransitionOnRange(greaterToOrEqual byte, lessThanOrEqual byte, nextState uint) error {
+	if greaterToOrEqual > lessThanOrEqual {
+		return fmt.Errorf("invalid range: %d must be less than or equal to %d", greaterToOrEqual, lessThanOrEqual)
+	} else if greaterToOrEqual == lessThanOrEqual {
+		crp.TransitionOnCharacter(greaterToOrEqual, nextState)
+		return nil
+	} else if greaterToOrEqual == 0 && lessThanOrEqual == 255 {
+		crp.setTransition(0, nextState)
 	}
 
-	rangeRoot := uint(0)
-	index := byte(0b1000_0000)
-	for ; index > 0; index = index >> 1 {
-		left := greaterToOrEqual & index
-		right := lessThan & index
+	upperBits := byte(0)
+	iter := byte(0b1000_0000)
+	for ; iter > 0; iter = iter >> 1 {
+		left := greaterToOrEqual & iter
+		right := lessThanOrEqual & iter
 
 		if left != right {
 			break
-		} else if left != 0 {
-			rangeRoot += uint(left)
+		} else {
+			upperBits += left
 		}
 	}
 
-	// so now we know greaterToOrEqual & index == 0
-	// and lessThan & index == 1
-	fullFromIndex := (index << 1) - 1
-	left := greaterToOrEqual & fullFromIndex
-	right := lessThan & fullFromIndex
+	lowerBits := (iter << 1) - 1
+	queue := make([]byte, 0, 256)
+	queue = append(queue, lowerBits)
+	for ; len(queue) > 0; iter = queue[0] {
+		lowest := upperBits
+		highest := upperBits + lowerBits
+		if greaterToOrEqual <= lowest && lessThanOrEqual >= highest {
+			crp.setTransition( /* TODO */ 0, nextState)
+		} else if greaterToOrEqual <= lowest {
+			queue = append(queue, index) // TODO
+		} else if lessThanOrEqual >= highest {
+			queue = append(queue, index) // TODO
+		}
+	}
 }
 
-func (crp *CharRangeProgression) GetTransitions(actual byte) []uint {
+func (crp *Progression) GetTransitions(actual byte) []uint {
 	result := make([]uint, 0, 32)
-	if crp.binaryTree[0] != nil {
-		for k, _ := range crp.binaryTree[0] {
+	if crp.binaryTreeOfSets[0] != nil {
+		for k, _ := range crp.binaryTreeOfSets[0] {
 			result = append(result, k)
 		}
 	}
@@ -65,8 +83,8 @@ func (crp *CharRangeProgression) GetTransitions(actual byte) []uint {
 			index = 2*index + 1
 		}
 
-		if crp.binaryTree[index] != nil {
-			for k, _ := range crp.binaryTree[0] {
+		if crp.binaryTreeOfSets[index] != nil {
+			for k, _ := range crp.binaryTreeOfSets[0] {
 				result = append(result, k)
 			}
 		}
