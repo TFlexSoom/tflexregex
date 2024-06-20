c5402bb4dac16e81e46befa17e9317d71ff6dda9
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2024-06-20 13:42:34 -0700

    Add some tests

--- lib/tflexregex/tflexregex.go
+++ lib/tflexregex/tflexregex.go
@@ -1,38 +1,53 @@
 package tflexregex
 
 type Regex struct {
+	definition string
 }
 
-type regexMatchMonad struct {
-	regex Regex
-	state uint
+// type regexMatchMonad struct {
+// 	regex Regex
+// 	state uint
+// }
+
+// func fromRegex(regex Regex) regexMonad {
+// 	return regexMonad{
+// 		regex:  regex,
+// 		founds: make([]uint, 0, 16),
+// 	}
+// }
+
+// func hasFound(monad regexMonad) bool {
+// 	return len(monad.founds) > 0
+// }
+
+// func match(monad regexMonad, next byte) regexMonad {
+
+// }
+
+// func monadMatch(regex Regex, b []byte) bool {
+// 	monad := fromRegex(regex)
+// 	for i := 0; i < len(b); i++ {
+// 		monad = match(monad, b[i])
+// 	}
+
+// 	return hasFound(monad)
+// }
+
+func Matches(pattern string, b []byte) (bool, error) {
+	return false, nil
 }
 
-func fromRegex(regex Regex) regexMonad {
-	return regexMonad{
-		regex:  regex,
-		founds: make([]uint, 0, 16),
-	}
+func (regex *Regex) Matches(b []byte) bool {
+	return false
 }
 
-func hasFound(monad regexMonad) bool {
-	return len(monad.founds) > 0
+func RegexFromString(pattern string) (Regex, error) {
+	regex := NewRegex()
+	// TODO fix
+	regex.definition = pattern
+	return regex, nil
 }
 
-func match(monad regexMonad, next byte) regexMonad {
-
-}
-
-// TODO MOVE elsewhere
-func Match(regex Regex, b []byte) bool {
-	monad := fromRegex(regex)
-	for i := 0; i < len(b); i++ {
-		monad = match(monad, b[i])
-	}
-
-	return hasFound(monad)
-}
-
-func NewInstance() Regex {
+func NewRegex() Regex {
 	return Regex{}
 }
--- lib/tflexregex/tflexregex_test.go
+++ lib/tflexregex/tflexregex_test.go
@@ -1,12 +1,69 @@
 package tflexregex
 
-/*
+import "testing"
 
-aabc
-0->1 a
-1->2 a
-2->3 b
-3->4 d
+func validate(res bool, err error, onInvalid func()) {
+	if err != nil || !res {
+		onInvalid()
+	}
+}
 
+func TestMatchAabc(t *testing.T) {
+	res, err := Matches("aabc", []byte("aabc"))
+	validate(res, err, func() { t.Errorf("result is not positive %v: %v", res, err) })
 
-*/
+	res, err = Matches("aabc", []byte("abc"))
+	validate(!res, err, func() { t.Errorf("result is not negative %v: %v", res, err) })
+}
+
+func TestMatchStar(t *testing.T) {
+	res, err := Matches("a*", []byte("aaaaaaaaaaa"))
+	validate(res, err, func() { t.Errorf("result is not positive %v: %v", res, err) })
+
+	res, err = Matches("a*", []byte{})
+	validate(res, err, func() { t.Errorf("result is not positive %v: %v", res, err) })
+
+	res, err = Matches("a*", []byte("a"))
+	validate(res, err, func() { t.Errorf("result is not positive %v: %v", res, err) })
+
+	res, err = Matches("ab*c*", []byte("abababccccccc"))
+	validate(res, err, func() { t.Errorf("result is not positive %v: %v", res, err) })
+
+	res, err = Matches("ab*", []byte("ababab"))
+	validate(res, err, func() { t.Errorf("result is not positive %v: %v", res, err) })
+
+	res, err = Matches("a*", []byte("b"))
+	validate(!res, err, func() { t.Errorf("result is not negative %v: %v", res, err) })
+
+	res, err = Matches("a*", []byte("aaaab"))
+	validate(!res, err, func() { t.Errorf("result is not negative %v: %v", res, err) })
+
+	res, err = Matches("ab*c*", []byte("abababcab"))
+	validate(!res, err, func() { t.Errorf("result is not negative %v: %v", res, err) })
+}
+
+func TestMatchPlus(t *testing.T) {
+	res, err := Matches("a+", []byte("aaaaaaaaaaa"))
+	validate(res, err, func() { t.Errorf("result is not positive %v: %v", res, err) })
+
+	res, err = Matches("a+", []byte{})
+	validate(!res, err, func() { t.Errorf("result is not negative %v: %v", res, err) })
+
+	res, err = Matches("a+", []byte("a"))
+	validate(res, err, func() { t.Errorf("result is not positive %v: %v", res, err) })
+
+	res, err = Matches("a+", []byte("b"))
+	validate(!res, err, func() { t.Errorf("result is not negative %v: %v", res, err) })
+
+	res, err = Matches("a+", []byte("aaaab"))
+	validate(!res, err, func() { t.Errorf("result is not negative %v: %v", res, err) })
+
+	res, err = Matches("ab+", []byte("aaaab"))
+	validate(!res, err, func() { t.Errorf("result is not negative %v: %v", res, err) })
+
+	res, err = Matches("ab+", []byte("abbbbb"))
+	validate(!res, err, func() { t.Errorf("result is not negative %v: %v", res, err) })
+
+	res, err = Matches("ab+", []byte("abababa"))
+	validate(!res, err, func() { t.Errorf("result is not negative %v: %v", res, err) })
+}
